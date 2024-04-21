8b7da0c06e757fd87ab2c36ba896c23c522601fe
Piotr Suchy <piotrsuchy6@tlen.pl>
2024-04-21 11:21:35 +0200

    Repo initialization

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tinynetlab - WIP
+
+Some experiments with spinning up simple network topologies using docker, containerlab and FRR for routing.
---
+++ setup_linode.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+$INSTALLATION_TYPE=$1
+
+# git + docker installation
+sudo apt update && sudo apt install -y git
+sudo ./docker_setup.sh
+
+if [ $INSTALLATION_TYPE=="FRR" ]; then
+    echo "One-host FRR installation"
+    git clone https://github.com/FRRouting/frr.git
+    cd frr
+    docker build -t frr-debian:11 -f docker/debian/Dockerfile .
+    docker run --privileged --init -itd frr-debian-11 bash
+else
+    echo "Full installation of containerlab and basic topology"
+    bash -c "$(curl -sL https://get.containerlab.dev)"
+    # containerlab create -t 3-tier-topology.yaml
+fi
