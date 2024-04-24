820f911074f47f42722c3b983616ed271707e1f9
Piotr Suchy <piotrsuchy@proton.me>
2024-04-24 22:26:52 +0200

    Another Dockerfile added

--- setup_linode.sh
+++ setup_linode.sh
@@ -11,7 +11,8 @@ if [ $INSTALLATION_TYPE=="FRR" ]; then
     git clone https://github.com/FRRouting/frr.git
     cd frr
     docker build -t frr-debian:11 -f docker/debian/Dockerfile .
-    docker run --privileged --init -itd frr-debian-11 bash
+    docker run --privileged --init --name frr-deb -itd frr-debian-11 bash
+	docker exec -it frr-deb git clone https://github.com/piotrsuchy/frr && bash
 else
     echo "Full installation of containerlab and basic topology"
     bash -c "$(curl -sL https://get.containerlab.dev)"
