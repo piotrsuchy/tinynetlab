8e87fddaef82919803a4487912a87187fc304adb
Piotr Suchy <piotrsuchy@proton.me>
2024-05-05 08:42:49 +0000

    Fixes to prep_frr script

--- topos/custom/frr_recursive/prep_frr_recursive.sh
+++ topos/custom/frr_recursive/prep_frr_recursive.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+HOST1=$1
+HOST2=$2
+HOST3=$3
+
 # HOST1:
-docker exec -c host1 ip address add 33.33.33.33/32 dev lo
-docker exec -c host1 ip route add 22.22.22.22 via 33.33.33.33 dev lo
+docker exec ${HOST1} /bin/bash -c 'ip address add 33.33.33.33/32 dev lo'
+docker exec ${HOST1} /bin/bash -c 'ip route add 22.22.22.22 via 33.33.33.33 dev lo'
+
+docker exec ${HOST1} /bin/bash -c 'ip link add dummy1 type dummy'
+docker exec ${HOST1} /bin/bash -c 'ip address add 20.20.20.20/32 dev dummy1'
+docker exec ${HOST1} /bin/bash -c 'ip link set dummy1 up'
 
 # HOST2:
-# docker exec -c host2 ip route 22.22.22.22 via 172.18.0.1 dev eth0
-docker exec -c host2 ip route add 33.33.33.33 via 172.18.0.1 dev eth0
+# docker exec -c ${HOST2} ip route add 33.33.33.33 via 20.20.20.20 dev eth0
 
 # HOST3:
-
+docker exec ${HOST3} /bin/bash -c 'ip address add 33.33.33.33/32 dev lo'
+docker exec ${HOST3} /bin/bash -c 'ip link add dummy1 type dummy'
+docker exec ${HOST3} /bin/bash -c 'ip address add 20.20.20.20/32 dev dummy1'
+docker exec ${HOST3} /bin/bash -c 'ip link set dummy1 up'
